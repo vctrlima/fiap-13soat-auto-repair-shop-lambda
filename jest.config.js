7214bb81dd3b5e3eb21429b054b7e035a208e865
Victor Lima <victor.limac@icloud.com>
2026-04-18 10:13:57 -0300

    fix: update jest config to include coverage thresholds and add ignoreDeprecations in tsconfig

--- jest.config.js
+++ jest.config.js
@@ -15,4 +15,12 @@ module.exports = {
   ],
   coverageDirectory: "coverage",
   coverageReporters: ["text", "lcov", "clover"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
